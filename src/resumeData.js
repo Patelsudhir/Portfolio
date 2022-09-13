4b4b61158f39beacf1f39d4c5e561b3a184eab28
[Sudhir Patel] <[patelsudhir03official@gmail.com]>
2022-09-14 01:03:24 +0530

    sc

---
+++ src/resumeData.js
@@ -0,0 +1,95 @@
+let resumeData = {
+  imagebaseurl: "http://github.com/patelsudhir/",
+  name: "Sudhir Patel",
+  role: "Frontend Developer and Competitive Coder",
+  linkedinId: "http://www.linkedin.com/in/sudhir-patel-430b75202/",
+  skypeid: "http://github.com/patelsudhir",
+  roleDescription: "",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "http://www.linkedin.com/in/sudhir-patel-430b75202/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "http://github.com/patelsudhir/",
+      className: "fa fa-github",
+    },
+    {
+      name: "skype",
+      url: "https://twitter.com/Sudhir_patel07",
+      className: "fa fa-twitter",
+    },
+  ],
+  aboutme:
+    "I am currently a final year student at Institute of Engineering and Technology, Lucknow and pursuing my Bachelors in Information Technology. I am a Competitive Coder and an enthusiast Web developer with knowledge of technology like HTML, CSS, JavaScript, React.js, Node.js.",
+  address: "Email : patelsudhir03official@gmail.com",
+  website: "Phone no : (+91) 7023347772",
+  education: [
+    {
+      UniversityName: "Institute of Engineering and Technology, Lucknow",
+      specialization: "Mechanical Engineering",
+      MonthOfPassing: "May",
+      YearOfPassing: "2023",
+      Achievements: "CGPA : 8.3",
+    },
+    {
+      UniversityName: "Indra K S iNTER College",
+      specialization: "PCM",
+      MonthOfPassing: "Apr",
+      YearOfPassing: "2018",
+      Achievements: "Percentage : 85%",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "SoftPro India",
+      specialization: "C, C++, Python, Matlab, Autocad",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2020",
+      Achievements: "",
+    },
+  ],
+  skillsDescription: "",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "JAVASCRIPT",
+    },
+    {
+      skillname: "REACTJS",
+    },
+  ],
+  portfolio: [
+    {
+      name: "Playing Pogo",
+      description: "Web Game",
+      imgurl: "images/portfolio/pogo-game.jpg",
+      // "projectlink":"https://mradul-dixit.vercel.app/"
+    },
+    {
+      name: "Chrome Extension",
+      description: "Web Application",
+      imgurl: "images/portfolio/chrome-extension.jpg",
+      // "projectlink":"https://github.com/mraduldixit"
+    },
+  ],
+  testimonials: [
+    {
+      description: "I am 3 star at codechef.",
+      name: "",
+    },
+    {
+      description: "I have done 200+ Questions on various coding platforms.",
+      name: "",
+    },
+  ],
+};
+
+export default resumeData;
