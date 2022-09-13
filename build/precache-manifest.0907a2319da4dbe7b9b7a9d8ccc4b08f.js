c2b006eb681b7acfcd9bb64082da88f5edff4b58
[Sudhir Patel] <[patelsudhir03official@gmail.com]>
2022-09-14 01:27:39 +0530

    s c

---
+++ build/precache-manifest.0907a2319da4dbe7b9b7a9d8ccc4b08f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "062cc2d471520708e197",
+    "url": "/static/css/main.efc4bd30.chunk.css"
+  },
+  {
+    "revision": "062cc2d471520708e197",
+    "url": "/static/js/main.062cc2d4.chunk.js"
+  },
+  {
+    "revision": "1048638a4991ffad96d3",
+    "url": "/static/js/1.1048638a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "486d5c0ee64732f6cdb3c9a42f34c76e",
+    "url": "/index.html"
+  }
+];
